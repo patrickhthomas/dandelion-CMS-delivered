59a1a2b42f9c843a2256477326dbc3dddb711266
Patrick <patrick@sendhelp.biz>
2021-05-29 17:08:20 -0700

    deletes browser.js

--- my-gatsby-project/gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-const React = require("react")
-const Layout = require("./src/components/Layout").default
-
-export function wrapPageElement({ element, props }) {
-  const Layout = element.type.Layout ?? React.Fragment
-  return <Layout {...props}>{element}</Layout>
-}
